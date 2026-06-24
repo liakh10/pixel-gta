50ea414b89b8e14fbb07d995cc9177e7231a0a46
liakh10 <119670997+liakh10@users.noreply.github.com>
2026-06-24 13:24:03 +0300

    Update config.ts

--- app/config.ts
+++ app/config.ts
@@ -4,7 +4,7 @@ export const GAME_CONFIG = {
   network: "mainnet-beta" as const,
 };
 
-export const CA: string = "SOON";           // Replace with real CA on launch
+export const CA: string = "COMING SOON";           // Replace with real CA on launch
 export const X_URL = "https://x.com/pixelgta_solana";
 export const TICKER = "$PIXELGTA";
 export const TOKEN_NAME = "PIXELGTA";
